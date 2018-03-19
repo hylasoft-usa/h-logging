737d8558cfddcc066979e462ad68c36d8521adc7
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2018-03-19 08:23:39 -0500

    Adds ILoggingCollection.

---
+++ h-logging/Loggers/Interfaces/ILoggingCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Hylasoft.Resolution;
+
+namespace Hylasoft.Logging.Loggers.Interfaces
+{
+  public interface ILoggingCollection : IResultLogger, IEnumerable<IResultLogger>
+  {
+    Result Add(params IResultLogger[] loggers);
+
+    Result Add(IEnumerable<IResultLogger> loggers);
+
+    Result Add(IResultLogger logger);
+
+    Result Remove(string logId);
+  }
+}
